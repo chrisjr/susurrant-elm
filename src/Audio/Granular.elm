54f40ae56ae3f67823e846f6f1e934a674cd5d76
Cora Johnson-Roberson <cora.johnson.roberson@gmail.com>
2015-06-25 23:40:44 -0400

    change module to tasks

--- src/Audio/Granular.elm
+++ src/Audio/Granular.elm
@@ -1,5 +1,6 @@
 module Audio.Granular where
 
+import Audio.Common exposing (..)
 import Audio.Probabilities exposing (..)
 import Debug
 import Html exposing (Html, text)
@@ -15,10 +16,6 @@ type alias Envelope =
     { attack : Float
     , release : Float
     }
-
-type Panning
-    = NoPan
-    | Pan Float   -- [-1, 1]
 
 type alias GrainParams =
     { transposition : Float
@@ -36,6 +33,7 @@ type alias Model = { buffer : Maybe AudioBuffer
                    , triggerSpread : Float
                    , offsets : Maybe (CDF TokenOffset)
                    , volume : Float
+                   , gainNodes : List GainNode
                    }
 
 type Action
@@ -46,15 +44,16 @@ type Action
     | PlayOffsets (Maybe (CDF TokenOffset))
 
 defaultEnvelope : Envelope
-defaultEnvelope = { attack = 0.4, release = 0.4 }
+defaultEnvelope = { attack = 0.2, release = 0.2 }
 
 defaultModel : Model
 defaultModel = { buffer = Nothing
                , envelope = defaultEnvelope
                , seed = Random.initialSeed 0
-               , bufferSpread = 0.5
+               , bufferSpread = 0.3
                , triggerSpread = 0.2
                , offsets = Nothing
+               , gainNodes = List.map (\_ -> makeGain 0.0) [1..numVoices]
                , volume = 0.7 }
 
 defaultGrainParams : GrainParams
@@ -67,40 +66,12 @@ defaultGrainParams =
     , envelope = defaultEnvelope
     }
 
-makeGain x =
-    createGainNode DefaultContext
-    |> tapNode .gain (setValue x)
-
-defaultGainNode = makeGain 0.7
-                |> connectNodes (getDestinationNode DefaultContext) 0 0
-
-mulParam : Float -> AudioParam -> AudioParam
-mulParam mul param = setValue (mul * getValue param) param
-
 totalTime {attack, release} = attack + release
 
 isJust : Maybe a -> Bool
 isJust x = case x of
              Just _ -> True
              Nothing -> False
-
-makeLinearPanner : Float -> PannerNode
-makeLinearPanner x =
-    createPannerNode DefaultContext
-        |> setPanningModel EqualPower
-        |> setDistanceModel Linear
-        |> setPosition x 0.0 0.0
-
-makePanner pan =
-    case pan of
-      Pan x -> Just <| makeLinearPanner x
-      NoPan -> Nothing
-
-maybeConnect : AudioNode a -> Maybe (AudioNode b) -> AudioNode c -> AudioNode a
-maybeConnect a b c =
-    case b of
-      Just b' -> a |> connectNodes b' 0 0 |> connectNodes c 0 0
-      Nothing -> a |> connectNodes c 0 0
 
 startSourceAndEnv source localGain envelope bufferOffset triggerAt amp =
     let dur = totalTime envelope
@@ -111,12 +82,11 @@ startSourceAndEnv source localGain envelope bufferOffset triggerAt amp =
         localGain' = localGain
                    |> tapNode .gain (linearRampToValue amp (start + envelope.attack))
                    |> tapNode .gain (linearRampToValue 0.0 (start + dur))
-    in localGain
+    in localGain'
 
 -- makeGrain : AudioBuffer -> GrainParams -> AudioNode a
-makeGrain buffer {transposition, pan, amp, bufferOffset, triggerAt, envelope} =
-    let localGain = makeGain 0.0
-        panner = makePanner pan
+makeGrain localGain buffer {transposition, pan, amp, bufferOffset, triggerAt, envelope} =
+    let panner = makePanner pan
         dur = totalTime envelope
         source = createAudioBufferSourceNode DefaultContext
                |> setAudioBufferForNode buffer
@@ -152,8 +122,8 @@ triggerGrains voices model =
     case model.buffer of
       Just buffer' ->
            let (offsets, seed') = makeRandomParams voices model
-               grainsWith = makeGrain buffer' << makeParams model
-               _ = List.map grainsWith offsets
+               grainsWith gain off = makeGrain gain buffer' <| makeParams model off
+               _ = List.map2 grainsWith model.gainNodes offsets
            in Signal.send actions.address (NewSeed seed')
       Nothing -> Task.succeed ()
 
@@ -167,10 +137,12 @@ update action model =
       PlayOffsets l -> { model | offsets <- l }
       NoOp -> model
 
-view = text << toString
+view = text << toString << .offsets
+
+numVoices = 8
 
 doAudio model =
-    triggerGrains 8 model
+    triggerGrains numVoices model
 
 actions : Signal.Mailbox Action
 actions = Signal.mailbox NoOp
@@ -178,7 +150,7 @@ actions = Signal.mailbox NoOp
 -- Signals
 
 audioBuffer : Signal (Maybe AudioBuffer)
-audioBuffer = loadAudioBufferFromUrl DefaultContext "/data/samples.wav"
+audioBuffer = loadAudioBufferFromUrl DefaultContext "/data/samples.mp3"
 
 bufferLoaded : Signal (Task x ())
 bufferLoaded =
@@ -189,7 +161,7 @@ model : Signal Model
 model = Signal.foldp update defaultModel actions.signal
 
 grainTrigger : Signal Time
-grainTrigger = every (100 * millisecond)
+grainTrigger = every (400 * millisecond)
 
 audioTasks : Signal (Task x ())
 audioTasks = model
@@ -200,11 +172,18 @@ playOffsets : Maybe (CDF TokenOffset) -> Task x ()
 playOffsets = Signal.send actions.address << PlayOffsets
 
 {-
+
 port bufferDone : Signal (Task x ())
 port bufferDone = bufferLoaded
 
 port audioDone : Signal (Task x ())
 port audioDone = audioTasks
+
+majorProbs = List.map (\x -> { offset = x * 1.0, prob = 1.0/3.0}) [0.0, 4.0, 7.0]
+major = toCDF majorProbs
+
+port startMajor : Task x ()
+port startMajor = playOffsets (Just major)
 
 main = view <~ model
 -}
